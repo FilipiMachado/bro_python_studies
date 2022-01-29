4df955735139a87243765abde5a9e6138a8c3fe5
filipi <filipisee@hotmail.com>
2022-01-29 17:39:05 -0300

    tests

--- bro-code-course/main.py
+++ bro-code-course/main.py
@@ -1,4 +1,8 @@
-first_name = "Zueirinho"
-last_name = "Rulez"
+#first_name = "Zueirinho "
+#last_name = "Rulez"
+#full_name = first_name + last_name 
+#print("Hello " + full_name)
 
-print("Hello " + name)
+age = 21
+age += 5
+print(age)
