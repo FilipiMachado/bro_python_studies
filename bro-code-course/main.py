b36dc0048da7d476982eaea3bffe258b10d4fff9
filipi <filipisee@hotmail.com>
2022-01-31 21:55:46 -0300

    small changes

--- bro-code-course/main.py
+++ bro-code-course/main.py
@@ -1,4 +1,4 @@
-#first_name = "Zueirinho "
+""" #first_name = "Zueirinho "
 #last_name = "Rulez"
 #full_name = first_name + last_name 
 #print("Hello " + full_name)
@@ -6,4 +6,13 @@
 height = 1.65
 ageStr = "21"
 print('Your height is: ' + str(height) + 'cm')
+
+ """
+
+""" # name = 'Zuerinho'
+# age = 25
+# smart = True
+name, age, smart = 'Zuerinho', 25, True
+
+print(name, smart) """
 
