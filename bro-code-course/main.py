fc553f598f0011b735a14d56065861f683b8b613
filipi <filipisee@hotmail.com>
2022-02-12 12:15:35 -0300

    type cast tests

--- bro-code-course/main.py
+++ bro-code-course/main.py
@@ -9,12 +9,16 @@ print('Your height is: ' + str(height) + 'cm')
 
  """
 
+""" ----------------------------------- """
+
 """ # name = 'Zuerinho'
 # age = 25
 # smart = True
 name, age, smart = 'Zuerinho', 25, True
 
 print(name, smart) """
+
+""" ----------------------------------- """
 
 #name = 'Fili Code'
 
@@ -26,4 +30,20 @@ print(name, smart) """
 # print(name.count('i'))
 #print(name.replace('i', 'o'))
 
+""" ----------------------------------- """
 
+# type casting = convert the data type of a value to another
+# data type
+
+x = 1   #int
+y = 2.0 #float
+z = "3" #str
+
+#x = str(x)
+y = int(y)
+y = str(y)
+z = float(z)
+
+#print("A variável X é: " + x)
+print("A variável Y é: " + y)
+# print(z * 3)
