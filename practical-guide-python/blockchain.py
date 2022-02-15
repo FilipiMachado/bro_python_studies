d2618b263d7fd152d8cda1d39e658558d6d838f6
filipi <filipisee@hotmail.com>
2022-02-14 22:28:32 -0300

    testing functions

--- practical-guide-python/blockchain.py
+++ practical-guide-python/blockchain.py
@@ -1,7 +1,30 @@
-blockchain = []
+blockchain = [[4]]
 
-def add_value():
-    blockchain.append("hello")
+def add_value(transaction_amount):
+    blockchain.append([blockchain[-1], transaction_amount])
+    print(blockchain)
 
-add_value(),
-print(blockchain)
+add_value(2.5),
+add_value(8),
+add_value(3.6),
+
+
+
+
+
+
+
+
+
+
+
+""" def sum(a, b):
+    return a + b
+
+print(sum(5, 15)) """
+
+
+""" def calculate(number):
+    print(25 + number)
+
+calculate(5) """
