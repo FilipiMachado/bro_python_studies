05e8e764d1371e2921415ce8d602de9eeb716de1
filipi <filipisee@hotmail.com>
2022-02-15 22:01:44 -0300

    small changes

--- practical-guide-python/blockchain.py
+++ practical-guide-python/blockchain.py
@@ -7,9 +7,11 @@ def get_last_blockchain_value():
 def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
-add_value(2.5)
-add_value(8, get_last_blockchain_value())
-add_value(3.6, get_last_blockchain_value())
+
+tx_amount = int(input("Your transaction amount please: "))
+add_value(tx_amount)
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=8)
+add_value(3.60, get_last_blockchain_value())
 
 print(blockchain)
 
