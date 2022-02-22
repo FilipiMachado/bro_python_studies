94c7be652cc9f350fcbf746dd71373c19b0076be
filipi <filipisee@hotmail.com>
2022-02-21 22:08:56 -0300

    loop tests

--- practical-guide-python/loop_exercises.py
+++ practical-guide-python/loop_exercises.py
@@ -1,5 +1,9 @@
-for i in range(1, 11):
-    if i % 2 == 0:
-        print('Par Number:', i)
-    else:
-        print('Ímpar Number:', i)
+# Exercise 1
+""" for i in range(1, 11):
+    print(i) """
+    
+# Exercise 2
+for i in range(1, 6, 1):
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    print()
