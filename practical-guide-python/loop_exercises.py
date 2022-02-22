90b70a7adcec63430e4b8a6602695cc67de6b486
filipi <filipisee@hotmail.com>
2022-02-21 22:26:58 -0300

    tests

--- practical-guide-python/loop_exercises.py
+++ practical-guide-python/loop_exercises.py
@@ -3,7 +3,15 @@
     print(i) """
     
 # Exercise 2
-for i in range(1, 6, 1):
+""" for i in range(1, 6, 1):
     for j in range(1, i + 1):
         print(j, end=' ')
-    print()
+    print() """
+    
+# Exercise 3
+j = 0
+n = int(input('Enter a value: '))
+
+for i in range(1, n + 1, 1):
+    j += i
+print(j)
