3d04a96a58cea75146ce5c0f25b8c92c47aa71c5
filipi <filipisee@hotmail.com>
2022-03-16 09:00:09 -0300

    test

---
+++ freeCodeCamp_exercises/conditional_b_ex1.py
@@ -0,0 +1,10 @@
+""" hours = int(input('Enter Hours: '))
+rate = int(input('Enter Rate: '))
+
+if hours > 40:
+    hours = hours * 1.5
+    print(hours)
+    
+pay = hours * rate
+print(pay) """
+
--- practical-guide-python/freeCodeCamp_exercises/conditional.py
+++
@@ -1,8 +0,0 @@
-x = 5
-
-if x < 10:
-    print('Smaller')
-if x > 2:
-    print('Bigger')
-
-print('Finish')
